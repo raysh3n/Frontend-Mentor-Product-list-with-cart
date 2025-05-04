687f0865b9f62993f4b266b5c69af34b68d59ca6
gfhf <fhgf>
2025-05-04 22:47:46 +0800

    moved assets & data.json to public folder, and edited the path from ./ to / in data.json for vite to work

--- src/eventListener.js
+++ src/eventListener.js
@@ -10,7 +10,7 @@ export const getEventStartLoad=function() {
     document.addEventListener('DOMContentLoaded',function(){
       console.log('hoshdoadhaohdad');
       
-      fetch('/data.json').then(response=>{
+      fetch('../data.json').then(response=>{
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
